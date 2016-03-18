eb72b0af4c94cd6d35cde78304e739645e97bb35
baifann <185758494@qq.com>
2016-03-18 16:10:34 +0800

    ui进行了改变

--- PlaySoundButton/app/src/main/java/com/baifan/playsoundbutton/MainActivity.java
+++ PlaySoundButton/app/src/main/java/com/baifan/playsoundbutton/MainActivity.java
@@ -10,17 +10,40 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    /**
+     * 播放声音的按钮
+     */
     private Button mBtnPlaySound;
 
+    /**
+     * 正在播放的按钮
+     */
     private ImageView mImgLoading;
 
+    /**
+     * 正在播放时需要的动画
+     */
     private ObjectAnimator mObjectAnimator;
 
+    /**
+     * 播放声音时的图片
+     */
     private ImageView mImgPlaySound;
+    /**
+     * 音乐描述
+     */
+    private TextView mTvSoundDescribe;
+
+    /**
+     * 删除音乐按钮
+     */
+    private ImageButton mImgDel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +58,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     private void initEvents() {
         mBtnPlaySound.setOnClickListener(this);
+        mImgDel.setOnClickListener(this);
     }
 
     /**
      * 初始化控件
      */
     private void initViews() {
+        mImgDel = (ImageButton) findViewById(R.id.img_del);
         mBtnPlaySound = (Button) findViewById(R.id.btn_playSound);
         mImgLoading = (ImageView) findViewById(R.id.img_loading);
         mImgPlaySound = (ImageView) findViewById(R.id.img_play);
+        mTvSoundDescribe = (TextView) findViewById(R.id.tv_sound_describe);
     }
 
     @Override
     public void onClick(View v) {
-        mBtnPlaySound.setEnabled(false);
-        startAnim();
-        startPlaySoundAnim();
-        mHandler.sendEmptyMessageDelayed(0, 5000);
+        switch (v.getId()) {
+            case R.id.btn_playSound:
+                mBtnPlaySound.setEnabled(false);
+                startAnim();
+                startPlaySoundAnim();
+                mHandler.sendEmptyMessageDelayed(0, 5000);
+                break;
+            case R.id.img_del:
+                Toast.makeText(this, "!!!!", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
     /**
@@ -85,9 +118,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 播放音频的动画
      */
-    private void startPlaySoundAnim(){
+    private void startPlaySoundAnim() {
         mImgPlaySound.setImageResource(R.drawable.anim_play_sound);
         AnimationDrawable animationDrawable = (AnimationDrawable) mImgPlaySound.getDrawable();
         animationDrawable.start();
+    }
+
+
+    private void setSoundDescribe(String text) {
+        mTvSoundDescribe.setVisibility(View.VISIBLE);
+        mTvSoundDescribe.setText(text);
     }
 }
